7e2976db4648221ca2d59cd442760e84857939eb
victor <victor.mendoza@tenco.com.mx>
2020-07-24 11:37:08 -0500

    Initial commit

---
+++ src/main/java/com/physicalsoftware/usuarios_example/UsuariosExampleApplication.java
@@ -0,0 +1,13 @@
+package com.physicalsoftware.usuarios_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsuariosExampleApplication.class, args);
+    }
+
+}
